9d0b66897b54e50b5586a4725a4dbcbcee3b77c3
lomaski <lomaski1986@gmail.com>
2026-03-03 17:32:32 -0300

    new file:   img/Vector.png 	new file:   img/bit.png 	new file:   img/br.png 	new file:   img/eua.png 	new file:   img/euro.png 	new file:   img/logo.gif 	new file:   img/uk.png 	new file:   index.html 	new file:   script.js 	new file:   style.css

---
+++ script.js
@@ -0,0 +1,144 @@
+const buttonConversion = document.querySelector('.button-conversion')
+const coin = document.querySelectorAll('.coin')
+
+
+function conValues(){
+    const inputValue = document.querySelector('.section-input')
+    const r1 = document.querySelector('.r1')
+    const r2 = document.querySelector('.r2')
+    const dolasToday = 5.17
+    const euroToday = 6.08
+    const realToday = 0.19
+    const libraToday = 6.93
+    const bitcoinToday = 353392.97
+    const cVal = inputValue.value / dolasToday
+    
+    //console.log(coin[0].value)
+
+
+    if(coin[0].value == "dolar"){
+        r1.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency", currency: 'USD'
+        }).format(dolasToday)
+    }
+    if(coin[0].value == "euro"){
+        r1.innerHTML = new Intl.NumberFormat("en", {
+            style: "currency", currency: 'EUR'
+        }).format(euroToday)
+    }
+    if(coin[0].value == "real"){
+        r1.innerHTML = new Intl.NumberFormat("pt-br", {
+            style: "currency", currency: 'BRL'
+        }).format(realToday)
+    }
+    if(coin[0].value == "libra"){
+        r1.innerHTML = new Intl.NumberFormat("en-GB", {
+            style: "currency", currency: 'GBP'
+        }).format(libraToday)
+    }
+    if(coin[0].value == "bitcoin"){
+        r1.innerHTML = new Intl.NumberFormat('en-US', {
+            style: "currency", currency: 'XBT',
+            minimumFractionDigits: 2, // Customize as needed, BTC often uses more
+            maximumFractionDigits: 8  // BTC can have up to 8 decimal places 
+        }).format(bitcoinToday)
+    }
+
+
+    if(coin[1].value == "dolar"){
+        r2.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency", currency: 'USD'
+        }).format(cVal)
+    }
+    if(coin[1].value == "euro"){
+        r2.innerHTML = new Intl.NumberFormat("en", {
+            style: "currency", currency: 'EUR'
+        }).format(inputValue.value / euroToday)
+    }
+    if(coin[1].value == "real"){
+        r2.innerHTML = new Intl.NumberFormat("pt-br", {
+            style: "currency", currency: 'BRL'
+        }).format(inputValue.value / realToday)
+    }
+    if(coin[1].value == "libra"){
+        r2.innerHTML = new Intl.NumberFormat("en-GB", {
+            style: "currency", currency: 'GBP'
+        }).format(inputValue.value / libraToday)
+    }
+    if(coin[1].value == "bitcoin"){
+        r2.innerHTML = new Intl.NumberFormat('en-US', {
+            style: "currency", currency: 'XBT',
+            minimumFractionDigits: 2, // Customize as needed, BTC often uses more
+            maximumFractionDigits: 8  // BTC can have up to 8 decimal places 
+        }).format(inputValue.value / bitcoinToday)
+    }
+
+
+}
+
+function change(){
+    //console.log('Ch-ch-ch-ch-changes')
+
+    const name1 = document.getElementById('name1')
+    const name2 = document.getElementById('name2')
+    const img = document.querySelectorAll('.moeda-div-img')
+
+    if (coin[0].value == 'dolar') {
+        name1.innerHTML = 'Dólar'
+        img[0].src = "img/eua.png"
+    }
+
+    if (coin[0].value == 'euro') {
+        name1.innerHTML = 'Euro'
+        img[0].src = "img/euro.png"
+    }
+
+    if (coin[0].value == 'real') {
+        name1.innerHTML = 'Real'
+        img[0].src = "img/br.png"
+    }
+
+    if (coin[0].value == 'libra') {
+        name1.innerHTML = 'Libra'
+        img[0].src = "img/uk.png"
+    }
+    
+    if (coin[0].value == 'bitcoin') {
+        name1.innerHTML = 'Bitcoin'
+        img[0].src = "img/bit.png"
+    }
+
+
+
+    if (coin[1].value == 'dolar') {
+        name2.innerHTML = 'Dólar'
+        img[1].src = "img/eua.png"
+    }
+
+    if (coin[1].value == 'euro') {
+        name2.innerHTML = 'Euro'
+        img[1].src = "img/euro.png"
+    }
+
+    if (coin[1].value == 'real') {
+        name2.innerHTML = 'Real'
+        img[1].src = "img/br.png"
+    }
+
+    if (coin[1].value == 'libra') {
+        name2.innerHTML = 'Libra'
+        img[1].src = "img/uk.png"
+    }
+    
+    if (coin[1].value == 'bitcoin') {
+        name2.innerHTML = 'Bitcoin'
+        img[1].src = "img/bit.png"
+    }
+
+    conValues()
+}
+
+
+coin[0].addEventListener('change', change)
+coin[1].addEventListener('change', change)
+buttonConversion.addEventListener('click', conValues)
